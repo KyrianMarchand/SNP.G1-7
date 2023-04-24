9216db35dc23ab0e33e851bf237e17fc8d4ad809
Tommy12789 <119518336+Tommy12789@users.noreply.github.com>
2023-04-24 11:10:57 +0200

    Update Member.java

---
+++ opinion/Member.java
@@ -0,0 +1,13 @@
+package opinion;
+
+public class Member {
+    private String login;
+    private String password;
+    private String profile;
+
+    public Member(String login, String password, String profile) {
+        this.login = login;
+        this.password = password;
+        this.profile = profile;
+    }
+}
