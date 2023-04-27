5a541c1adf9952ca84dc3bfded30860978e68fc9
Kyriannn <72595101+Kyriannn@users.noreply.github.com>
2023-04-27 10:16:03 +0200

    Create Book.java
    Create class Book

---
+++ opinion/Book.java
@@ -0,0 +1,19 @@
+package opinion;
+
+public class Book {
+    private String title;
+    private String kind;
+    private String author;
+    private int nbPages;
+
+    public Book(String title, String kind, String author, int nbPages){
+        this.author = author;
+        this.kind = kind;
+        this.author = author;
+        this.nbPages = nbPages;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+}
