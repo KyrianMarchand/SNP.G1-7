d9e96e0ca2b11d1511036e6d624dac890f25f15d
Kyriannn <72595101+Kyriannn@users.noreply.github.com>
2023-05-12 10:24:06 +0200

    getFIlm + reviewItemFilm + change toString

--- opinion/SocialNetwork.java
+++ opinion/SocialNetwork.java
@@ -32,8 +32,7 @@ public class SocialNetwork implements ISocialNetwork {
 
     @Override
     public int nbFilms() {
-        // TODO Auto-generated method stub
-        return 0;
+        return films.size();
     }
 
     @Override
@@ -50,7 +49,6 @@ public class SocialNetwork implements ISocialNetwork {
      * @throws BadEntryException
      * @throws MemberAlreadyExistsException
      */
-
     @Override
     public void addMember(String login, String password, String profile)
             throws BadEntryException, MemberAlreadyExistsException {
@@ -75,6 +73,20 @@ public class SocialNetwork implements ISocialNetwork {
         members.add(newMember);
     }
 
+    /**
+     * Adds a new film to the library.
+     * 
+     * @param login
+     * @param password
+     * @param title
+     * @param kind
+     * @param director
+     * @param scenarist
+     * @param duration
+     * @throws BadEntryException
+     * @throws NotMemberException
+     * @throws ItemFilmAlreadyExistsException
+     */
     @Override
     public void addItemFilm(String login, String password, String title,
             String kind, String director, String scenarist, int duration)
@@ -135,7 +147,7 @@ public class SocialNetwork implements ISocialNetwork {
      * @param login
      * @param password
      * @param title
-     * @param kind     the kind of the book to add
+     * @param kind
      * @param author
      * @param nbPages
      * @throws BadEntryException
@@ -177,12 +189,73 @@ public class SocialNetwork implements ISocialNetwork {
         books.add(newBook);
     }
 
+    /**
+     * @param login
+     * @param password
+     * @param title
+     * @param mark
+     * @param comment
+     * @throws BadEntryException
+     * @throws NotMemberException
+     * @throws NotItemException
+     * @return mean of the marks for this film
+     */
     @Override
     public float reviewItemFilm(String login, String password, String title,
             float mark, String comment) throws BadEntryException,
             NotMemberException, NotItemException {
-        // TODO Auto-generated method stub
-        return 0;
+
+        if (login == null || login.trim().length() < 1)
+            throw new BadEntryException("Invalid login");
+        if (password == null || password.trim().length() < 4)
+            throw new BadEntryException("Invalid password");
+        if (title == null || title.trim().length() < 1)
+            throw new BadEntryException("Invalid book title");
+        if (mark < 0.0f || mark > 5.0f)
+            throw new BadEntryException("Invalid mark");
+        if (comment == null)
+            throw new BadEntryException("Invalid comment");
+        Member member = this.getMember(login);
+        if (member == null || !member.getPassword().equals(password)) {
+            throw new NotMemberException("The password does not match with the login of a registered member.");
+        }
+        Film film = this.getFilm(title);
+        if (film == null)
+            throw new NotItemException("Unknown film");
+        Review review = null;
+        for (Review r : member.getReviewMemberList()) {
+            if (r.getItem().equals(film)) {
+                review = r;
+                break;
+            }
+        }
+        if (review == null) {
+            review = new Review(mark, member, comment, film);
+            member.getReviewMemberList().add(review);
+            film.getReviewItemList().add(review);
+        } else {
+            review.setMark(mark);
+            review.setComment(comment);
+        }
+        float avgMark = 0.0f;
+        for (Review r : film.getReviewItemList()) {
+            avgMark += r.getMark();
+        }
+        avgMark /= film.getReviewItemList().size();
+        return avgMark;
+    }
+
+    /**
+     * @param title
+     * @return the film with the given title, or null if it does not exist
+     */
+    public Film getFilm(String title) throws NotItemException {
+        for (Film film : films) {
+            if (film.getTitle().toLowerCase().equalsIgnoreCase(title)) {
+                return film;
+            }
+        }
+        throw new NotItemException("Book not found in the social network.");
     }
 
     /**
@@ -198,6 +271,17 @@ public class SocialNetwork implements ISocialNetwork {
         throw new NotItemException("Book not found in the social network.");
     }
 
+    /**
+     * @param login
+     * @param password
+     * @param title
+     * @param mark
+     * @param comment
+     * @throws BadEntryException
+     * @throws NotMemberException
+     * @throws NotItemException
+     * @return mean of the marks for this book
+     */
     @Override
     public float reviewItemBook(String login, String password, String title, float mark, String comment)
             throws BadEntryException, NotMemberException, NotItemException {
@@ -212,16 +296,13 @@ public class SocialNetwork implements ISocialNetwork {
             throw new BadEntryException("Invalid mark");
         if (comment == null)
             throw new BadEntryException("Invalid comment");
-
         Member member = this.getMember(login);
         if (member == null || !member.getPassword().equals(password)) {
             throw new NotMemberException("The password does not match with the login of a registered member.");
         }
-
         Book book = this.getBook(title);
         if (book == null)
             throw new NotItemException("Unknown book");
-
         Review review = null;
         for (Review r : member.getReviewMemberList()) {
             if (r.getItem().equals(book)) {
@@ -229,27 +310,27 @@ public class SocialNetwork implements ISocialNetwork {
                 break;
             }
         }
-
         if (review == null) {
             review = new Review(mark, member, comment, book);
             member.getReviewMemberList().add(review);
             book.getReviewItemList().add(review);
-        }
-
-        else {
+        } else {
             review.setMark(mark);
             review.setComment(comment);
         }
-
         float avgMark = 0.0f;
         for (Review r : book.getReviewItemList()) {
             avgMark += r.getMark();
         }
         avgMark /= book.getReviewItemList().size();
-
         return avgMark;
     }
 
+    /**
+     * @param title
+     * @throws BadEntryException
+     * @return title and score by name
+     */
     @Override
     public LinkedList<String> consultItems(String title) throws BadEntryException {
         if (title == null || title.trim().length() == 0) {
@@ -269,15 +350,25 @@ public class SocialNetwork implements ISocialNetwork {
         return result;
     }
 
+    /**
+     * @return Members + Books + Films
+     */
     public String toString() {
         String result = "";
-        result += "Members: ";
-        for (Member member : members) {
-            result += member.getLogin() + "\n";
+        result += "Members:\n";
+        for (int i = 0; i < members.size(); i++) {
+            Member member = members.get(i);
+            result += "- " + member.getLogin() + "\n";
         }
-        result += "Books: ";
-        for (Book book : books) {
-            result += book.getTitle() + "\n";
+        result += "\nBooks:\n";
+        for (int i = 0; i < books.size(); i++) {
+            Book book = books.get(i);
+            result += "- " + book.getTitle() + " - Score: " + book.getAverageScore() + "\n";
+        }
+        result += "\nFilms:\n";
+        for (int i = 0; i < films.size(); i++) {
+            Film film = films.get(i);
+            result += "- " + film.getTitle() + " - Score: " + film.getAverageScore() + "\n";
         }
         return result;
     }
@@ -306,6 +397,7 @@ public class SocialNetwork implements ISocialNetwork {
             System.out.println(sn.consultItems("la"));
             sn.reviewItemBook("Kyrian", "kyrian", "La boulangerie", 2, "Enfaite moyen...");
             System.out.println(sn.consultItems("La boulangerie"));
+            System.out.println(sn.toString());
         } catch (NotMemberException | BadEntryException | NotItemException e) {
             e.printStackTrace();
         }
